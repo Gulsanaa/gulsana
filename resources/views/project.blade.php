7cff9b7bd0021dc2ce46178f95a4b2a69efebc6e
Gulsanaa <70758609+Gulsanaa@users.noreply.github.com>
2021-04-19 13:54:00 +0600

    create route file

---
+++ resources/views/project.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chatset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="menu">
+<div id="coffee" onclick="change(this)" onmouseover="big(this)" onmouseout="small(this)">Coffee</div>
+<div id="order online" onclick="change(this)" onmouseover="big(this)" onmouseout="small(this)" >Order Online</div>
+<div id="about us" onclick="change(this)" onmouseover="big(this)" onmouseout="small(this)" >About Us</div>
+<div id="contacts" onclick="change(this)" onmouseover="big(this)" onmouseout="small(this)">Contacts</div>
+<img  onclick ="window.location.reload()" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQOd7OC_3hdLSpG3wl85YgcP9CJ8bkSGkzJZw&usqp=CAU" alt="coffee" width="40px">
+</div>
+
+<div class="img">
+    <img id="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdwC8jSMth9MBAOjn3jsVtg0Nw6F1TgCxQ1w&usqp=CAU" alt="coffee" width="300px">
+    <h1 id="text0">Welcome to our coffee shop!</h1>
+    <p id="text1">Lorem ipsum dolor sit amet consectetur adipisicing elit.Commodi accusantium repellat molestias.Tempora quod animi voluptatem fugit tempore obcaecati quos amet labore dolorum quae.Ut id animi nesciunt pariatur reprehenderit.</p>
+</div>
+
+
+    <script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
